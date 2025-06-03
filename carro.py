3b6c3ebaf9369b2f8fcebd916c5f6b4c32b92549
martins-gu <moraisgustavohenrique02@gmail.com>
2025-06-03 16:29:09 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,9 @@
+nome_carro = input("Digite o nome do carro:")
+valor_carro = float(input("Digite o valor do carro:"))
+consumo_por_litro =float(input("Digite o valor por litro:"))
+
+print("--------------------------------------------")
+print(f"|Carro: {nome_carro}")
+print(f"|Valor: {valor_carro}")
+print(f"|Consumo por litro: {consumo_por_litro}")
+print("--------------------------------------------")
